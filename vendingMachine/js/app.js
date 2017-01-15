a6b8e6351c67bc85def0cd3e9d49bd9dbd32e7ac
callieliza <calidesigns42@gmail.com>
2017-01-15 16:23:47 -0500

    working on tut

--- vendingMachine/js/app.js
+++ vendingMachine/js/app.js
@@ -9,11 +9,19 @@ var Quarter = (function () {
     function Quarter() {
         this.value = .25;
     }
+    Object.defineProperty(Quarter.prototype, "Value", {
+        get: function () {
+            return this.value;
+        },
+        enumerable: true,
+        configurable: true
+    });
     Quarter.prototype.getImageUrl = function () {
         return "img/Quarter.png";
     };
     return Quarter;
 }());
+var coin = new Quarter();
 /**
  * Created by chewy on 1/14/17.
  */
--- vendingMachine/ts/coin.ts
+++ vendingMachine/ts/coin.ts
@@ -3,8 +3,13 @@
  */
 
 class Quarter {
-    value: number = .25;
+    private value: number = .25;
+    get Value() {
+        return this.value;
+    }
     getImageUrl(): string {
         return "img/Quarter.png";
     }
 }
+
+    var coin = new Quarter();
